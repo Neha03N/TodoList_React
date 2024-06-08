ad82699aa91d5264bf3f5c9fb0f9d643060910a9
Neha03N <nikhadeneha4@gmail.com>
2024-06-09 00:32:02 +0530

    Updates

---
+++ precache-manifest.c8eefea095b5ed7aed9c1c583906baab.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d88b6b4fdddb944394992a0cedce90c",
+    "url": "/TodoList_React/index.html"
+  },
+  {
+    "revision": "a732339b257473e39c28",
+    "url": "/TodoList_React/static/css/2.a7c3e6bb.chunk.css"
+  },
+  {
+    "revision": "f30b373b66471f96b124",
+    "url": "/TodoList_React/static/css/main.6c9cf8ac.chunk.css"
+  },
+  {
+    "revision": "a732339b257473e39c28",
+    "url": "/TodoList_React/static/js/2.ba1b80fd.chunk.js"
+  },
+  {
+    "revision": "4dd206dec5ff825e678a",
+    "url": "/TodoList_React/static/js/3.2ebb6cf0.chunk.js"
+  },
+  {
+    "revision": "f30b373b66471f96b124",
+    "url": "/TodoList_React/static/js/main.5a4fc677.chunk.js"
+  },
+  {
+    "revision": "5164491add4f950f771b",
+    "url": "/TodoList_React/static/js/runtime~main.7d596d6f.js"
+  },
+  {
+    "revision": "6bc7124a0d46a88b4d4f4fd0c1cfc7c1",
+    "url": "/TodoList_React/static/media/chacked2.6bc7124a.jpg"
+  },
+  {
+    "revision": "b55b1345f0b919f0cab774ec25d6654e",
+    "url": "/TodoList_React/static/media/fa-brands-400.b55b1345.woff2"
+  },
+  {
+    "revision": "b7dee83cb5ee2c47b053e2620f4bbb78",
+    "url": "/TodoList_React/static/media/fa-brands-400.b7dee83c.ttf"
+  },
+  {
+    "revision": "3c264849ff4eb9b6e99eab9cd54c80ae",
+    "url": "/TodoList_React/static/media/fa-regular-400.3c264849.ttf"
+  },
+  {
+    "revision": "aa7c5fa494807f7a9ec907defee083e8",
+    "url": "/TodoList_React/static/media/fa-regular-400.aa7c5fa4.woff2"
+  },
+  {
+    "revision": "0a95f951745ba02faa8773ea6a1ebaed",
+    "url": "/TodoList_React/static/media/fa-solid-900.0a95f951.ttf"
+  },
+  {
+    "revision": "1ec0ba058c021acf7feaa18081445d63",
+    "url": "/TodoList_React/static/media/fa-solid-900.1ec0ba05.woff2"
+  },
+  {
+    "revision": "95b97efa98f9e3fb869bc9634c43a0cc",
+    "url": "/TodoList_React/static/media/fa-v4compatibility.95b97efa.ttf"
+  },
+  {
+    "revision": "fdb652dcc200dd23b8b8040176858c36",
+    "url": "/TodoList_React/static/media/fa-v4compatibility.fdb652dc.woff2"
+  },
+  {
+    "revision": "9c05c9299a85f316bfaea6b7b4e68b97",
+    "url": "/TodoList_React/static/media/unchecked.9c05c929.png"
+  }
+]);
